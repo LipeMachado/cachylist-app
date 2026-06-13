d2077cc8b9a80a0aef73238af8d8894f7da2e0f2
LipeMachado <silvalipe765@gmail.com>
2026-06-13 10:50:43 -0300

    Puma single mode (workers 0) to save RAM; hide auth-art on login page

--- config/puma.rb
+++ config/puma.rb
@@ -31,6 +31,10 @@ threads threads_count, threads_count
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
 
+# Single mode (workers 0) saves memory by avoiding app duplication via fork.
+# SolidQueue runs in-process and doesn't need separate workers.
+workers 0
+
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
